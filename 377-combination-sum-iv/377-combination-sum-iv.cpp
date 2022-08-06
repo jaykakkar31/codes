55893ae2de61d2b285fa07c07f19c7be62e67cb3
Jay Kakkar <69637792+jaykakkar31@users.noreply.github.com>
2022-08-06 22:45:25 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (30.10%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    
+     int help(vector<int> &nums, int T, int n,vector<int> &dp){
+		// base case: if target is 0, we found combination then return 1
+        if(T==0) return 1;
+        int ans=0; 
+		
+		// memoisation
+        if(dp[T]!=-1) return dp[T];
+		
+		// picking up each elements less than target and 
+		// calling this function recursively
+        for(int i=0; i<n; i++){
+            if(nums[i]<=T){
+                ans+=help(nums, T-nums[i], n,dp);
+            }
+        }
+        return dp[T]= ans;
+    }
+    int combinationSum4(vector<int>& nums, int target) {
+        int n=nums.size();
+		// resizing the dp array to store values from 0 to target
+        vector<int> dp(target+1,-1);
+        // dp.resize(target+1,-1);
+        return help(nums, target, n,dp);
+    }
+};
