49df30377ffe814c2a5ce4165c8f0a256b6c2287
Jay Kakkar <69637792+jaykakkar31@users.noreply.github.com>
2022-08-08 19:03:17 +0530

    Time: 324 ms (52.18%), Space: 81.4 MB (28.34%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -3,14 +3,15 @@ public:
     int findCenter(vector<vector<int>>& edges) {
         // vector<vector<in
         int n=edges.size();
-        vector<vector<int>> adj(n+2);
+        vector<int> adj[n+2];
+        // vector<vector<int>> adj(n+2);
         for(int i=0;i<n;i++){
             adj[edges[i][0]].push_back(edges[i][1]);
             adj[edges[i][1]].push_back(edges[i][0]);
             
         }
         
-        for(int i=0;i<=adj.size();i++){
+        for(int i=0;i<=n+1;i++){
             if(adj[i].size()==n){
                 return i;
             }
