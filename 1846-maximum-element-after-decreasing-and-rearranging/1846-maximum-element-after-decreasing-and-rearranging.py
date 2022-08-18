645a20543f21b38bc1e29073972b2950e3e65f5e
Jay Kakkar <69637792+jaykakkar31@users.noreply.github.com>
2022-08-18 22:56:23 +0530

    Time: 1077 ms (6.93%), Space: 24 MB (81.44%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.py
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.py
@@ -5,18 +5,11 @@ class Solution:
         count=0
         if arr[0]!=1:
             # print("R")
-            if 1 not in arr:
-                arr[0]=1
-                count+=1
-            else:
-                ind=arr.index(1)
-        
-                arr[0],arr[ind]=arr[ind],arr[0]
-                count+=1
+            arr[0]=1
             
         # print(arr)
         for i in range(1,len(arr)):
             if abs(arr[i]-arr[i-1])>1:
                 arr[i]=arr[i-1]+1
-                count+=1
+                # count+=1
         return max(arr)
