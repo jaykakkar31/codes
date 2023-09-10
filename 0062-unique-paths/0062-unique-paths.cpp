59c0f1eda67aa2835c3d69fd004f01b8f8ff64fb
Jay Kakkar <69637792+jaykakkar31@users.noreply.github.com>
2023-09-10 20:56:08 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (56.20%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -11,23 +11,18 @@ public:
         
     }
     
-    int solve(int m,int n,int i,int j,vector<vector<int>> &dp){
-        if(i==(n-1)&&j==(m-1)) return 1;
-        else if(i>=n||j>=m) return 0;
-        else if(dp[i][j]!=-1) return dp[i][j];
-        else
-            return dp[i][j]= solve(m,n,i+1,j,dp)+solve(m,n,i,j+1,dp);
-    }
+    
     int uniquePaths(int m, int n) {
        
        
+        
+        
+        
         vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
        
-        //dp[1][1]=1;
        int num=countPaths(0,0,m,n,dp);
         if(m==1&&n==1)
-            return num;
+            return 1;
         return dp[0][0];
-        // return count;
     }
 };
