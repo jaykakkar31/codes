61998fafa241fe14cd624e650d0aa4c9870bb7f1
Jay Kakkar <69637792+jaykakkar31@users.noreply.github.com>
2023-12-25 10:57:03 +0000

    Time: 7 ms (88.72%), Space: 8.4 MB (58.57%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,16 +9,33 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        // ListNode *fast=head,*slow=head;
-        vector<ListNode*> ans;
-        while(head){
-            if (find(ans.begin(),ans.end(),head->next)!=ans.end()){
+        ListNode *fast=head,*slow=head;
+        // if(slow==fast){
+        //     return false;
+        // }
+        if(slow==NULL){
+            return false;
+        }
+        while(fast&&fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
                 return true;
-            }else{
-                ans.push_back(head);
             }
-            head=head->next;
+            
         }
         return false;
+        
+        
+        // vector<ListNode*> ans;
+        // while(head){
+        //     if (find(ans.begin(),ans.end(),head->next)!=ans.end()){
+        //         return true;
+        //     }else{
+        //         ans.push_back(head);
+        //     }
+        //     head=head->next;
+        // }
+        // return false;
     }
 };
