facda48dd863e41aa09ad0e5e67bc86aaa01c449
Jay Kakkar <69637792+jaykakkar31@users.noreply.github.com>
2024-04-01 23:12:36 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (18.88%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,24 +1,57 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        int l1=word1.size(),l2=word2.size(),i=0,j=0  ;
-        string s="";
-        while(i<l1&&j<l2){
+        
+        int i=0,j=0;
+        string ans="";
+        while(i<word1.size()&&j<word2.size()){
             if(i==j){
-                s+=word1[i];
-                i++;
+                    ans+=word1[i];
+                i+=1;
             }else{
-                s+=word2[j];
-                j++;
+                ans+=word2[j];
+                j+=1;
             }
+            
+            
         }
-        while (i<l1){
-            s+=word1[i];
-            i++;
-        }while(j<l2){
-            s+=word2[j];
-            j++;
+        while(i<word1.size()){
+            ans+=word1[i];
+            i+=1;
         }
-        return s;
-    }
+        while(j<word2.size()){
+            ans+=word2[j];
+            j+=1;
+        }
+        return ans;
+    
+        
+        
+        }   
+        
+        
+        
+        
+        
+        
+    //     int l1=word1.size(),l2=word2.size(),i=0,j=0  ;
+    //     string s="";
+    //     while(i<l1&&j<l2){
+    //         if(i==j){
+    //             s+=word1[i];
+    //             i++;
+    //         }else{
+    //             s+=word2[j];
+    //             j++;
+    //         }
+    //     }
+    //     while (i<l1){
+    //         s+=word1[i];
+    //         i++;
+    //     }while(j<l2){
+    //         s+=word2[j];
+    //         j++;
+    //     }
+    //     return s;
+    // }
 };
